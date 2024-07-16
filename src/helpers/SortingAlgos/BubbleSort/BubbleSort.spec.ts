764a3156fb535655f67ff4d874a241c4dc8456da
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-07-16 21:52:13 +0530

    bubble sort test cases added

--- src/helpers/SortingAlgos/BubbleSort/BubbleSort.spec.ts
+++ src/helpers/SortingAlgos/BubbleSort/BubbleSort.spec.ts
@@ -1,6 +1,7 @@
 import { describe, test } from 'vitest';
 import generateArray from '../../RandomNoArray/randomNoArray';
 import { bubbleSort, swap } from './BubbleSort';
+import { bubbleSortAnim } from './BubbleAnim';
 
 describe('swap test', () => {
   test('test swap', async () => {
@@ -12,10 +13,19 @@ describe('swap test', () => {
 });
 
 describe('bubbleSort test', () => {
-  test.only('test bubbleSort', async () => {
+  test('test bubbleSort', async () => {
     const array = generateArray(5);
     console.log({ array });
     const op = bubbleSort(array);
+    console.log({ op });
+  });
+});
+
+describe('bubbleSort Anim test', () => {
+  test.only('test bubbleSort', async () => {
+    const array = generateArray(5);
+    console.log({ array });
+    const op = bubbleSortAnim(array);
     console.log({ op });
   });
 });
